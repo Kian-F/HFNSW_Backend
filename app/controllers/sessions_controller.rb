327dffcc6d6fcf5c2155c77d4f411b7628d7084c
Kian-F <52603628+Kian-F@users.noreply.github.com>
2021-06-29 23:10:53 +1000

    change session_store

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -32,4 +32,9 @@ class SessionsControllers < ApplicationController
       }
     end
   end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_out: true }
+  end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
+if Rails.env == "production"
 Rails.application.config.session_store :cookie_store, key: "_HFNSW", domain: "jdh-HFNSW-app-api.herokuapp.com"
+else 
+  Rails.application.config.session_store :cookie_store, key: "_HFNSW"
+end
